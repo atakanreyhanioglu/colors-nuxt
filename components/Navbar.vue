674bbc5dabb3cd04fe8ea1354f918d2013096d45
atakanreyhanioglu <atakan.reyhanioglu@gmail.com>
2022-03-20 23:16:37 +0300

    commit

---
+++ components/Navbar.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="navbar">
+    <div class="announce"><span id="text">WEBCOLORS</span></div>
+    <div class="logo">
+      <img src="~/assets/logo.png" alt="logo"/>
+    </div>
+    <div class="member">
+      <ui-button><span><b>LOGIN</b></span></ui-button>
+      <span>or&nbsp;&nbsp;</span>
+      <ui-button id="register" outlined icon="brush"><span><b>REGISTER</b></span></ui-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavbarComponent',
+}
+</script>
+
+<style>
+.navbar {
+  width: 80%;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+.logo {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.logo img {
+  width: 24%;
+}
+#register {
+  border: solid 2px #C099F8;
+  height: 60px;
+}
+#text{
+  font-family: Yesteryear, cursive;
+  text-align: center;
+  font-size: 35px;
+  color: rgb(27, 13, 15);
+  background-color: rgba(84, 84, 84, 0);
+  text-shadow: rgba(48, 33, 33, 0) 2px 2px 2px;
+}
+.member {
+  justify-content: space-between;
+}
+.member span {
+  font-style: italic;
+}
+</style>
---
+++ layouts/Content.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="content">
+    <div class="left-side">
+      <div class="title"><h2>Try WEBCOLORS for free and get colors from amazing pages</h2></div>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      A asperiores aspernatur assumenda atque
+      commodi corporis dolor doloremque enim error
+      fugiat iste laboriosam laborum molestias nam nihil
+      nisi odio omnis quaerat, quam quo quod, quos reiciendis, sapiente tempore temporibus voluptatem voluptatibus.
+    </div>
+    <div class="right-side">
+    </div>
+    <div class="page-url">
+      <ui-textfield outlined>
+        Page url
+      </ui-textfield>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContentLayout',
+}
+</script>
+<style>
+.content {
+  width: 100%;
+  height: 700px;
+}
+.title h2 {
+  color: grey;
+}
+</style>
